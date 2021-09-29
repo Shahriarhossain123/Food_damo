71dc42c1de916e317c772e85f0f7c4f58b7a0c6a
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-09-29 19:44:28 +0600

    template2

--- food_item/views.py
+++ food_item/views.py
@@ -8,11 +8,10 @@ from django.template import loader
 
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
     context = {
         'item_list': item_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
