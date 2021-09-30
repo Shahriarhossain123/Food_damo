a5cb4ac206f651f10b36a37f2a18dfa5e3a91c3d
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-09-30 20:06:57 +0600

    static2

--- food_item/models.py
+++ food_item/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     name = models.CharField("Item Name", max_length=200)
     desc = models.CharField("Descriptions", max_length=200)
     price = models.IntegerField("Item Price")
+    image = models.CharField("Image", max_length=500)
 
     def __str__(self):
         return self.name
