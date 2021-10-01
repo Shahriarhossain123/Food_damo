3b0ba83ddfce475d7813b658f32dc439775b12d5
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-10-01 23:01:50 +0600

    Add item2

--- food_item/urls.py
+++ food_item/urls.py
@@ -3,9 +3,16 @@ from django.urls import path
 
 app_name = 'food'
 urlpatterns = [
+    # food
     path('', views.index, name='index'),
+    # food/id
     path('<int:item_id>/', views.detail, name='detail'),
+    # item
     path('item/', views.item, name='item'),
-
+    # add
     path('add', views.create_item, name='create_item'),
+    # update
+    path('update/<int:id>', views.update_item, name='update_item'),
+    # Delete
+    path('delete/<int:id>', views.delete_item, name='delete_item'),
 ]
