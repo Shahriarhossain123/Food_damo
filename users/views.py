35d6bd617bd765d76173c23967193b932764bde5
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-10-02 18:04:41 +0600

    users3

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegisterForm
@@ -16,3 +17,8 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
